3d2c18e2baa3a49b911ca236f88188ce75d360a3
Rhinossoross <menadue.daniel@gmail.com>
2026-01-14 14:35:05 +0000

    Split function into setup - loop - cleanup structure for faster mid program running

--- Aruco/aruco detector external use example.py
+++ Aruco/aruco detector external use example.py
@@ -1,8 +1,14 @@
-from ArucoDetector import GetMarkerAngle
+from ArucoDetector import GetMarkerAngle, GetCameraData, CleanupCamera
 
-success, angles = GetMarkerAngle(cameraID = 0,showVideo=True, averagingItertions=10, continuous=False)
-if success:
-    for i in range(len(angles)):
-        print(f"Angle between marker {i} & {i+1}: {angles[i]} degrees")
-else:
-    print("No markers detected.")
+GotData,CameraData = success,data = GetCameraData(cameraID =0)
+
+if GotData:
+    for x in range (10):
+        GotAngles,angles = GetMarkerAngle(CameraData, maxIterations=10, averagingItertions=5)
+        if GotAngles:
+            for i in range(len(angles)):
+                print(f"Angle between marker {i} & {i+1}: {angles[i]} degrees")
+        else:
+            print("No markers detected.")
+        print("#################################")
+CleanupCamera(CameraData)
